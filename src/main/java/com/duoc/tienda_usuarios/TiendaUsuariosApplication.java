34dbc7a4fdd2fa24cbea1d866f4c37f87e6b4d9f
espanderlof <jaime717@hotmail.es>
2024-10-25 13:29:23 -0300

    Inicio

---
+++ src/main/java/com/duoc/tienda_usuarios/TiendaUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.tienda_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaUsuariosApplication.class, args);
+	}
+
+}
